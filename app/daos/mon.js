9a7a55cc378db8654751861f5072d58d78f1280f
xinshangshangxin <xinshangshangxin@gmail.com>
2016-03-18 21:27:34 +0800

    feat: 添加mongodb连接openshift的配置

--- app/daos/mon.js
+++ app/daos/mon.js
@@ -1,74 +1,46 @@
 'use strict';
 
+var _ = require('lodash');
 var mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-
 var path = require('path');
 
-var dbName = 'ngGather';
+mongoose.Promise = require('bluebird');
 
-function getCodingMongodbUri() {
-  try {
-    return JSON.parse(process.env.VCAP_SERVICES).mongodb[0].credentials.uri;
+var dbName = process.env.dbName || 'noDbName';
+
+var optionConfig = {
+  coding: {
+    type: 'fun',
+    fun: getCodingMongodbUri
+  },
+  docker: {
+    type: 'env',
+    condition: 'host',
+    username: 'MONGO_USERNAME',
+    password: 'MONGO_PASSWORD',
+    host: 'MONGO_PORT_27017_TCP_ADDR',
+    post: 'MONGO_PORT_27017_TCP_PORT',
+    name: 'MONGO_INSTANCE_NAME'
+  },
+  daoCloud: {
+    type: 'env',
+    condition: 'host',
+    username: 'MONGODB_USERNAME',
+    password: 'MONGODB_PASSWORD',
+    host: 'MONGODB_PORT_27017_TCP_ADDR',
+    post: 'MONGODB_PORT_27017_TCP_PORT',
+    name: 'MONGODB_INSTANCE_NAME'
+  },
+  openshift: {
+    type: 'env',
+    condition: 'host',
+    username: 'OPENSHIFT_MONGODB_DB_USERNAME',
+    password: 'OPENSHIFT_MONGODB_DB_PASSWORD',
+    host: 'OPENSHIFT_MONGODB_DB_HOST',
+    post: 'OPENSHIFT_MONGODB_DB_PORT',
+    name: 'OPENSHIFT_APP_NAME'
   }
-  catch(e) {
-    return false;
-  }
-}
-
-function getDockerMongo(database) {
-  var mongodbUri = 'mongodb://';
-
-  if(process.env.MONGO_USERNAME) {
-    mongodbUri += process.env.MONGO_USERNAME;
-
-    if(process.env.MONGO_PASSWORD) {
-      mongodbUri += ':' + process.env.MONGO_PASSWORD;
-    }
-    mongodbUri += '@';
-  }
-
-  if(!process.env.MONGO_PORT_27017_TCP_ADDR) {
-    return false;
-  }
-
-  mongodbUri += (process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost') + ':' + (process.env.MONGO_PORT_27017_TCP_PORT || 27017) + '/' + (process.env.MONGO_INSTANCE_NAME || database || 'test');
-
-  return mongodbUri;
-}
-
-function getDaoCloudorLocalMongodbUri(database) {
-  // 链接格式:    mongodb://user:pass@localhost:port/database
-  // DaoCloud链接地址
-  var mongodbUri = 'mongodb://';
-
-  if(process.env.MONGODB_USERNAME) {
-    mongodbUri += process.env.MONGODB_USERNAME;
-
-    if(process.env.MONGODB_PASSWORD) {
-      mongodbUri += ':' + process.env.MONGODB_PASSWORD;
-    }
-    mongodbUri += '@';
-  }
-
-  mongodbUri += (process.env.MONGODB_PORT_27017_TCP_ADDR || 'localhost') + ':' + (process.env.MONGODB_PORT_27017_TCP_PORT || 27017) + '/' + (process.env.MONGODB_INSTANCE_NAME || database || 'test');
-
-  return mongodbUri;
-}
-
-function getMongodbUri() {
-  var _args = arguments;
-  // 返回函数 为了添加参数database
-  return function(database) {
-    for(var i = 0, l = _args.length; i < l; i++) {
-      var fn = _args[i];
-      var uri = fn(database);
-      if(uri !== false) {
-        return uri;
-      }
-    }
-  };
-}
+};
 
 function close() {
   mongoose.connection.close(function() {
@@ -76,14 +48,129 @@ function close() {
   });
 }
 
+function getCodingMongodbUri() {
+  try {
+    return _.get(JSON.parse(process.env.VCAP_SERVICES), 'mongodb[0].credentials.uri');
+  }
+  catch(e) {
+    return false;
+  }
+}
 
-var mongodbUri = getMongodbUri(getCodingMongodbUri, getDockerMongo, getDaoCloudorLocalMongodbUri)(dbName);
+function resolveEnvUrl(config) {
+  console.log(config.condition, config[config.condition], process.env[config[config.condition]]);
+  if(config.condition && !process.env[config[config.condition]]) {
+    return false;
+  }
 
-//console.log(mongodbUri);
+  var mongodbUri = 'mongodb://';
+  if(process.env[config.username]) {
+    mongodbUri += process.env[config.username];
+
+    if(process.env[config.password]) {
+      mongodbUri += ':' + process.env[config.password];
+    }
+    mongodbUri += '@';
+  }
+
+  mongodbUri += (process.env[config.host] || 'localhost') + ':' + (process.env[config.port] || 27017) + '/' + (process.env[config.name] || dbName);
+
+  return mongodbUri;
+}
+
+function getMongodbUri() {
+  var _args = arguments;
+  return function() {
+    for(var i = 0, l = _args.length; i < l; i++) {
+      var config = optionConfig[_args[i]];
+      if(!config) {
+        continue;
+      }
+
+      var uri = '';
+      switch(config.type) {
+        case 'fun':
+        {
+          uri = config.fun();
+          break;
+        }
+        case 'env':
+        {
+          uri = resolveEnvUrl(config);
+          break;
+        }
+        default:
+        {
+          break;
+        }
+      }
+
+      if(uri !== false) {
+        return uri;
+      }
+    }
+
+    return 'mongodb://localhost:27017/' + dbName;
+  };
+}
+
+
+var mongodbUri = getMongodbUri('openshift', 'coding')();
 
 // 链接数据库
 var db = mongoose.connect(mongodbUri);
 
-module.exports.db = db;
-module.exports.mongoose = mongoose;
-module.exports.close = close;
+function generateNewMongooseType(modelName, opt) {
+  var modelNameSchema = new mongoose.Schema(_.assign({
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updatedAt: {
+      type: Date
+    }
+  }, opt));
+
+  // 更新 updatedAt
+  modelNameSchema.pre('save', function(next) {
+    this.updatedAt = Date.now();
+    next();
+  });
+
+  modelNameSchema.pre('update', function(next) {
+    this.updatedAt = Date.now();
+    next();
+  });
+
+  modelNameSchema.pre('findOneAndUpdate', function(next) {
+    if(!this.createdAt) {
+      this.findOneAndUpdate({}, {
+        createdAt: Date.now()
+      });
+    }
+    this.findOneAndUpdate({}, {
+      updatedAt: Date.now()
+    });
+    next();
+  });
+  var modelNameModel = db.model(modelName, modelNameSchema);
+
+  modelNameSchema.set('toJSON', {
+    transform: function(doc, ret) {
+      ret.id = ret._id;
+    }
+  });
+
+  return {
+    model: modelNameModel,
+    schema: modelNameSchema
+  };
+}
+
+module.exports = {
+  mongoose: mongoose,
+  Types: mongoose.Schema.Types,
+  db: db,
+  generateNewMongooseType: generateNewMongooseType,
+  close: close
+};
